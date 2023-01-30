c2ab9435dfe9d6f300741e1643fe05f9791ed6b3
Omar Vega <omarvega598@gmail.com>
2023-01-30 13:56:18 -0500

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,19 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <div className='Score'>
+        <div>
+          ROCK <br /> PAPER <br /> SCISSORS
+        </div>
+        <div className='Score_label'>
+          <span>SCORE</span>
+          <span>12</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
